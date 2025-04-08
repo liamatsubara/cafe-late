9e0f4c2f9f480d77f346930598e322bd4a225703
perkles <matsubaralia@gmail.com>
2025-04-07 23:41:15 -0300

    :sparkles: navbar e footer

---
+++ src/app/vitrine/vitrine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vitrine',
+  imports: [],
+  templateUrl: './vitrine.component.html',
+  styleUrl: './vitrine.component.css'
+})
+export class VitrineComponent {
+
+}
