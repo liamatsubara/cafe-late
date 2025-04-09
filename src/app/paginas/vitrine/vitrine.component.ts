4f0285814438ec46c617cdcb758b08c5b2780a4d
Natalia Taira <nataliagtaira@gmail.com>
2025-04-09 17:31:11 -0300

    feat: adicionadas imagens dos produtos mockados

--- src/app/paginas/vitrine/vitrine.component.ts
+++ src/app/paginas/vitrine/vitrine.component.ts
@@ -11,12 +11,12 @@ import { CartaoProdutoComponent } from '../../componentes/cartao-produto/cartao-
 
 export class VitrineComponent {
   produtos = [
-    { nome: 'Reserva Caramelo', preco: 52.00, imagem: 'assets/img/produto1.png' },
-    { nome: 'Bulldogue Bourbon', preco: 52.00, imagem: 'assets/img/produto2.png' },
-    { nome: 'Cerrado Pastor', preco: 52.00, imagem: 'assets/img/produto3.png' },
-    { nome: 'Golden Sul de Minas', preco: 49.00, imagem: 'assets/img/produto4.png' },
-    { nome: 'Mogiana Labrador', preco: 49.00, imagem: 'assets/img/produto5.png' },
-    { nome: 'Terrier da Mantiqueira', preco: 49.00, imagem: 'assets/img/produto6.png' },
+    { nome: 'Reserva Caramelo', preco: 52.00, imagem: '/produtos/produto1.png' },
+    { nome: 'Bulldogue Bourbon', preco: 52.00, imagem: 'produtos/produto2.png' },
+    { nome: 'Cerrado Pastor', preco: 52.00, imagem: 'produtos/produto3.png' },
+    { nome: 'Golden Sul de Minas', preco: 49.00, imagem: 'produtos/produto4.png' },
+    { nome: 'Mogiana Labrador', preco: 49.00, imagem: 'produtos/produto5.png' },
+    { nome: 'Terrier da Mantiqueira', preco: 49.00, imagem: 'produtos/produto6.png' },
   ];
 }
 
