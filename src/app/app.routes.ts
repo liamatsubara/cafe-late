2e53b83b061000ded878d6d18a7622a308110c33
Natalia Taira <nataliagtaira@gmail.com>
2025-04-09 01:21:22 -0300

    feat: adicionar arquivo de rotas com rota inicial para VitrineComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VitrineComponent } from './paginas/vitrine/vitrine.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:VitrineComponent}
+];
