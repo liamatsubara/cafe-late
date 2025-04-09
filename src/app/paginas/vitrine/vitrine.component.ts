1229f087707ca45c721533af035ff5c828f8c920
Natalia Taira <nataliagtaira@gmail.com>
2025-04-09 15:11:57 -0300

    feat(vitrine): adiciona ao componente Vitrine uma lista de produtos mockados

--- src/app/paginas/vitrine/vitrine.component.ts
+++ src/app/paginas/vitrine/vitrine.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartaoProdutoComponent } from '../../componentes/cartao-produto/cartao-produto.component';
 
 @Component({
   selector: 'app-vitrine',
-  imports: [],
+  imports: [CommonModule, CartaoProdutoComponent],
   templateUrl: './vitrine.component.html',
   styleUrl: './vitrine.component.css'
 })
-export class VitrineComponent {
 
+export class VitrineComponent {
+  produtos = [
+    { nome: 'Reserva Caramelo', preco: 52.00, imagem: 'assets/img/produto1.png' },
+    { nome: 'Bulldogue Bourbon', preco: 52.00, imagem: 'assets/img/produto2.png' },
+    { nome: 'Cerrado Pastor', preco: 52.00, imagem: 'assets/img/produto3.png' },
+    { nome: 'Golden Sul de Minas', preco: 49.00, imagem: 'assets/img/produto4.png' },
+    { nome: 'Mogiana Labrador', preco: 49.00, imagem: 'assets/img/produto5.png' },
+    { nome: 'Terrier da Mantiqueira', preco: 49.00, imagem: 'assets/img/produto6.png' },
+  ];
 }
+
