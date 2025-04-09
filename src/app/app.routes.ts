43f0db8baae04986f5e5c1fe7c0c4e5d934b500c
Natalia Taira <nataliagtaira@gmail.com>
2025-04-09 18:37:10 -0300

    feat: adicionada rota para o login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { VitrineComponent } from './paginas/vitrine/vitrine.component';
+import { LoginComponent } from './paginas/login/login.component';
 
 export const routes: Routes = [
-    {path:"", component:VitrineComponent}
+    {path:"", component:VitrineComponent},
+    {path:"login", component:LoginComponent}
 ];
