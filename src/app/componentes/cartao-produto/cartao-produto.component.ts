0c63fca56716d1268e195c982f17fb0899555390
perkles <matsubaralia@gmail.com>
2025-04-10 11:20:34 -0300

    Correção cards vitrine

--- src/app/componentes/cartao-produto/cartao-produto.component.ts
+++ src/app/componentes/cartao-produto/cartao-produto.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { CarrinhoService } from '../../service/carrinho.service'; // ajuste o caminho se necessário
+import { Produto } from '../../model/produto';
 
 @Component({
   selector: 'app-cartao-produto',
@@ -8,16 +10,43 @@ import { Router } from '@angular/router';
   styleUrl: './cartao-produto.component.css',
   standalone: true
 })
-
 export class CartaoProdutoComponent {
   @Input() id!: number;
   @Input() nome!: string;
   @Input() preco!: number;
   @Input() imagem!: string;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private carrinhoService: CarrinhoService
+  ) {}
 
   irParaDetalhe() {
     this.router.navigate(['/detalhe', this.id]);
+  }
+
+  comprar() {
+    const produto: Produto = {
+      id: this.id,
+      nome: this.nome,
+      preco: this.preco,
+      imagem: this.imagem,
+      descricao: '',
+      origem: '',
+      peso: '',
+      perfilSensorial: {
+        variedade: '',
+        processo: '',
+        altitude: '',
+        torra: '',
+        corpo: '',
+        acidez: '',
+        docura: '',
+        idealPara: ''
+      }
+    };
+
+    this.carrinhoService.adicionarProduto(produto);
+    alert('Produto adicionado ao carrinho!');
   }
 }
