d08d041d4a7256077592c7c4586efa16b0446897
Natalia Taira <nataliagtaira@gmail.com>
2025-04-09 22:26:01 -0300

    feat: criação de rota para cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { VitrineComponent } from './paginas/vitrine/vitrine.component';
 import { DetalheComponent } from './paginas/detalhe/detalhe.component';
 import { LoginComponent } from './paginas/login/login.component';
+import { CadastroComponent } from './paginas/cadastro/cadastro.component';
 
 export const routes: Routes = [
     {path:"", component:VitrineComponent},
     {path:"login", component:LoginComponent},
-    {path:"detalhe", component:DetalheComponent}
+    {path:"detalhe", component:DetalheComponent},
+    {path:"cadastro", component:CadastroComponent}
 ];
