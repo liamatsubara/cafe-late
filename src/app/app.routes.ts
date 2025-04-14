8df912343b7e31af693cf3a3d51c1ec258b0aae9
Natalia Taira <nataliagtaira@gmail.com>
2025-04-14 15:03:37 -0300

    feat: criação de componente esqueci a senha e rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './paginas/login/login.component';
 import { CadastroComponent } from './paginas/cadastro/cadastro.component';
 import { BuscaComponent } from './paginas/busca/busca.component';
 import { CarrinhoComponent } from './paginas/carrinho/carrinho.component';
+import { EsqueciSenhaComponent } from './paginas/esqueci-senha/esqueci-senha.component';
 
 export const routes: Routes = [
     {path:"", component:VitrineComponent},
     {path:"login", component:LoginComponent},
     {path:"detalhe/:id", component:DetalheComponent},
     {path:"cadastro", component:CadastroComponent},
-    { path: 'busca', component: BuscaComponent },
-    { path: 'carrinho', component: CarrinhoComponent },
-
+    {path: 'busca', component: BuscaComponent },
+    {path: 'carrinho', component: CarrinhoComponent },
+    {path: 'esqueci-senha', component:EsqueciSenhaComponent}
 ];
---
+++ src/app/paginas/esqueci-senha/esqueci-senha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueci-senha',
+  imports: [],
+  templateUrl: './esqueci-senha.component.html',
+  styleUrl: './esqueci-senha.component.css'
+})
+export class EsqueciSenhaComponent {
+
+}
