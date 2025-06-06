93114bfbfbb8419b9e0aeccab41706f1b3d46802
Natalia Taira <nataliagtaira@gmail.com>
2025-06-05 22:42:58 -0300

    Adiciona base do projeto

---
+++ backend/src/main/java/com/fatec/cafe_late/CafeLateApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.cafe_late;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeLateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeLateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fatec/cafe_late/CafeLateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.cafe_late;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeLateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
