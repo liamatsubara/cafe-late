cf3c27b68cf7fe52a327800458a9e166e8059b72
Natalia Taira <nataliagtaira@gmail.com>
2025-06-09 20:13:40 -0300

    Implementa a controller de Pedido com todos métodos CRUD

--- backend/src/main/java/com/fatec/cafe_late/controller/PedidoController.java
+++ backend/src/main/java/com/fatec/cafe_late/controller/PedidoController.java
@@ -1,4 +1,41 @@
 package com.fatec.cafe_late.controller;
 
+import com.fatec.cafe_late.entity.Pedido;
+import com.fatec.cafe_late.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pedidos/")
 public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @PostMapping
+    public Pedido criaPedido(@RequestBody Pedido pedido) {
+        return pedidoService.criar(pedido);
+    }
+
+    @GetMapping("{id}")
+    public Pedido obterPedidoPorId(@PathVariable Long id) {
+        return pedidoService.obterPedido(id);
+    }
+
+    @GetMapping
+    public List<Pedido> obterPedidos() {
+        return pedidoService.obterPedidos();
+    }
+
+    @PutMapping
+    public Pedido atualizarPedido(Pedido pedido) {
+        return pedidoService.atualizar(pedido);
+    }
+
+    @DeleteMapping
+    public void deletePedido(Long id) {
+        pedidoService.deletar(id);
+    }
 }
