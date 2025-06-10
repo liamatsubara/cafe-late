460211687101cb9fd66870d368a19c996c7052be
Natalia Taira <nataliagtaira@gmail.com>
2025-06-09 22:05:57 -0300

    Remove arquivo

--- backend/src/main/java/com/fatec/cafe_late/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fatec.cafe_late.repository;
-
-import com.fatec.cafe_late.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Pedido, Long> {
-}
