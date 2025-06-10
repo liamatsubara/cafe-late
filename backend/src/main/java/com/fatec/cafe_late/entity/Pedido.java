b305dc8f0504b6ebf89dee1b0da61dc7954ebdf8
Natalia Taira <nataliagtaira@gmail.com>
2025-06-09 21:33:25 -0300

    Corrije Pedido, implementando corretamente seus atributos e relacionamentos

--- backend/src/main/java/com/fatec/cafe_late/entity/Pedido.java
+++ backend/src/main/java/com/fatec/cafe_late/entity/Pedido.java
@@ -1,8 +1,7 @@
 package com.fatec.cafe_late.entity;
 
 import jakarta.persistence.*;
-
-import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "pedido")
@@ -11,28 +10,14 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    private String nome;
-    private BigDecimal preco;
-    private String imagem;
-    private String origem;
-    private String descricao;
-    private String peso;
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "perfil_sensorial_id", referencedColumnName = "id")
-    private PerfilSensorial perfilSensorial;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     public Pedido() {}
-    public Pedido(Long id, String nome, BigDecimal preco, String imagem, String origem, String descricao, String peso, PerfilSensorial perfilSensorial) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.imagem = imagem;
-        this.origem = origem;
-        this.descricao = descricao;
-        this.peso = peso;
-        this.perfilSensorial = perfilSensorial;
-    }
 
     public Long getId() {
         return id;
@@ -42,59 +27,19 @@ public class Pedido {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public List<ItemPedido> getItens() {
+        return itens;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
     }
 
-    public BigDecimal getPreco() {
-        return preco;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
-    public String getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(String imagem) {
-        this.imagem = imagem;
-    }
-
-    public String getOrigem() {
-        return origem;
-    }
-
-    public void setOrigem(String origem) {
-        this.origem = origem;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getPeso() {
-        return peso;
-    }
-
-    public void setPeso(String peso) {
-        this.peso = peso;
-    }
-
-    public PerfilSensorial getPerfilSensorial() {
-        return perfilSensorial;
-    }
-
-    public void setPerfilSensorial(PerfilSensorial perfilSensorial) {
-        this.perfilSensorial = perfilSensorial;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 }
