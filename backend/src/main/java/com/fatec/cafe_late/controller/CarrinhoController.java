b3a78f04b5006ce62e7f18bff368d940f913ea43
Natalia Taira <nataliagtaira@gmail.com>
2025-06-15 21:15:38 -0300

    Corrije endpoint

--- backend/src/main/java/com/fatec/cafe_late/controller/CarrinhoController.java
+++ backend/src/main/java/com/fatec/cafe_late/controller/CarrinhoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/carrinho")
+@RequestMapping("/carrinho")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CarrinhoController {
 
--- backend/src/main/java/com/fatec/cafe_late/controller/UsuarioController.java
+++ backend/src/main/java/com/fatec/cafe_late/controller/UsuarioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/usuario")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
